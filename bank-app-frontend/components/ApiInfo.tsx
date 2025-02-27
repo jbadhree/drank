a2c881c0ed67798689d496ccaa3dab6a0cb0b7c1
Badhree Babu <jbadhree@gmail.com>
2025-02-26 22:19:07 -0500

    Removed API info

--- bank-app-frontend/components/ApiInfo.tsx
+++ bank-app-frontend/components/ApiInfo.tsx
@@ -1,15 +1,8 @@
 import React from 'react';
 
+// Empty component now that we don't need the debug info
 const ApiInfo: React.FC = () => {
-  // Using the same hardcoded URL that will be replaced at container startup
-  const apiUrl = 'http://localhost:8080/api/v1';
-
-  return (
-    <div className="fixed bottom-2 right-2 bg-gray-100 border border-gray-300 p-2 rounded text-xs text-gray-700">
-      <div><strong>API URL:</strong> {apiUrl}</div>
-      <div className="text-xs italic mt-1">If this shows localhost but you're on a remote server, the URL replacement failed.</div>
-    </div>
-  );
+  return null;
 };
 
 export default ApiInfo;
